9277b272d3dd4a98d87908b1b2bd4b1293441065
Federico Tomassetti <federico@tomassetti.me>
2020-06-01 16:29:05 +0200

    add github action for tests

--- src/test/kotlin/RpgToIntermediateTest.kt
+++ src/test/kotlin/RpgToIntermediateTest.kt
@@ -1,8 +1,5 @@
 import com.strumenta.kolasu.parsing.toStream
-import com.strumenta.rpgtojava.intermediateast.GGlobalVariable
-import com.strumenta.rpgtojava.intermediateast.GIntegerType
-import com.strumenta.rpgtojava.intermediateast.GProgram
-import com.strumenta.rpgtojava.intermediateast.GStringType
+import com.strumenta.rpgtojava.intermediateast.*
 import com.strumenta.rpgtojava.transformRpgToIntermediate
 import com.strumenta.rpgtojava.transpileRpgToJava
 import org.junit.jupiter.api.Assertions
@@ -15,6 +12,13 @@ class RpgToIntermediateTest {
     fun simpleGlobalVariable() {
         val program = transformRpgToIntermediate("""     D NBR             S              8  0""".toStream(), "Test")
         assertEquals(GProgram("Test", globalVariables = mutableListOf(GGlobalVariable("NBR", GIntegerType))), program)
+    }
+
+    @Test
+    fun simpleFunctions() {
+        val program = transformRpgToIntermediate("""     C     FIB           BEGSR
+     C                   ENDSR""".toStream(), "Test")
+        assertEquals(GProgram("Test", otherFunctions = mutableListOf(GFunction("FIB"))), program)
     }
 
 }
